0d9c7954914ae90d657c6c18425370f9520a957a
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-23 02:08:43 -0300

    adicionando construtor com conexão com o banco de dados

--- core/controller.php
+++ core/controller.php
@@ -1,8 +1,21 @@
 <?php
 
-class controller{
+class controller extends model{
+
+	public function __construct(){
+		parent::__construct(); // conectar com banco de dados
+	}
 
 	public function carregarTemplate($view, $dados){
+		$menu = array();
+
+		$sql = "SELECT ID, TITULO FROM CATEGORIAS";
+		$sql = $this->db->query($sql);
+		
+		if($sql->rowCount()>0){
+			$menu = $sql->fetchAll();
+		}
+
 		include DIRETORIO.'/views/template.php';
 	}
 
