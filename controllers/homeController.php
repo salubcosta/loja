372cd8926870512ff8c66d3a501c1119d383e765
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-23 03:03:48 -0300

    implementação action index para listagem de produtos

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -3,6 +3,12 @@
 class homeController extends controller{
 
 	public function index(){
-		$this->carregarTemplate('home',[]);
+		$dados = array();
+
+		$produtos = new produtos();
+
+		$dados['produtos'] = $produtos->listar(8);
+
+		$this->carregarTemplate('home',$dados);
 	}
 }
