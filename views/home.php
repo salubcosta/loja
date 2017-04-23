b6e3f00fc80154d8919726acd890cd4d08e9e639
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-23 03:04:15 -0300

    recebendo produtos e organizando html

--- views/home.php
+++ views/home.php
@@ -1,1 +1,12 @@
-home
+<?php foreach($produtos as $prod): ?>
+<a href="<?php echo URL;?>/produto/ver/<?php echo $prod['ID'];?>">
+<div class="produto">
+	<img src="" width="180" height="180" border="0">
+	<strong><?php echo $prod['NOME'];?></strong><br />
+	R$ <?php echo $prod['PRECO'];?>
+
+</div>
+</a>
+<?php endforeach;?>
+
+<div style="clear: both;"></div>
