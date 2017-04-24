fa78bb04741c88ce2ce82bfe0116791571d6b4de
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-24 01:16:51 -0300

    improve

--- views/categoria.php
+++ views/categoria.php
@@ -3,7 +3,7 @@
 <?php foreach($produtos as $prod): ?>
 <a href="<?php echo URL;?>/produto/ver/<?php echo $prod['ID'];?>">
 <div class="produto">
-	<img src="" width="180" height="180" border="0">
+	<img src="<?php echo URL;?>/assets/images/prods/<?php echo $prod['IMAGEM'];?>" width="176" height="176" border="0">
 	<strong><?php echo $prod['NOME'];?></strong><br />
 	R$ <?php echo $prod['PRECO'];?>
 
