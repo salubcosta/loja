30212672c46a09d6124f8fb09fbea56400f156bb
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-24 01:15:12 -0300

    improve

--- controllers/categoriaController.php
+++ controllers/categoriaController.php
@@ -2,17 +2,25 @@
 
 class categoriaController extends controller{
 
-	public function ver($id){
-		$dados = array(
-			'categoria' => '',
-			'produtos' => array()
-			);
+	public function ver($id = ''){
+		if(!empty($id)){
+			$dados = array(
+				'categoria' => '',
+				'produtos' => array()
+				);
 
-		$categoria = new categorias();
-		$produtos = new produtos();
-		$dados['produtos'] = $produtos->listarCategoria(addslashes($id));
-		$dados['categoria'] = $categoria->getCategarias(addslashes($id));
-
-		$this->carregarTemplate('categoria', $dados);
+			$categoria = new categorias();
+			$produtos = new produtos();
+			$dados['produtos'] = $produtos->listarCategoria(addslashes($id));
+			$dados['categoria'] = $categoria->getCategarias(addslashes($id));
+			if(!empty($dados['categoria']) && !empty($dados['produtos'])){
+				$this->carregarTemplate('categoria', $dados);
+			}else{
+				$this->carregarTemplate('naoencontrado',[]);	
+			}
+			
+		}else{
+			$this->carregarTemplate('naoencontrado',[]);
+		}
 	}
 }
