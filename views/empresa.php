6f9c6da8765a8cab70b514b1ccf5f343125ab442
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-05-13 00:26:28 -0300

    add view empresa

---
+++ views/empresa.php
@@ -0,0 +1,11 @@
+<h1>Loja - <small>Salumão.com.br</small></h1>
+<p>Suas compras com segurança e com mais humanidade!</p>
+<hr>
+<h3><strong>Como tudo começou: </strong></h3>
+<p>Eis um belo dia, Salumão estava entediado, não estava fazendo nada de tão legal, então ele começou a escrever alguns códigos e ocmo passe de mágica, A Loja - Salumão.com.br estava escrita.</p>
+<p>Este projeto trata-se de uma loja virtual acrescido da função de pagamento implementado.</p>
+<p>Este projeto também está voltado a estudos...</p>
+<h3>Dúvidas:</h3>
+<p>
+	<a href="mailto:salubcosta@gmail.com">salubcosta@gmail.com</a>
+</p>
